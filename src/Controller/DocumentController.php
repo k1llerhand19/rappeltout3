0d3f39a64f6cd7f5a1239f0e8d5db2a3ff013cce
k1llerhand19 <135227972+k1llerhand19@users.noreply.github.com>
2023-09-19 15:06:03 +0200

    pdf upload

--- src/Controller/DocumentController.php
+++ src/Controller/DocumentController.php
@@ -14,6 +14,9 @@ use App\Form\DocumentFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
 class DocumentController extends AbstractController
 {
     #[Route('/document', name: 'app_document')]
@@ -25,12 +28,33 @@ class DocumentController extends AbstractController
     }
 
     #[Route('/admin/document/ajout', name: 'document.add')]
-    public function AjouterActuRequest(Request $request,  EntityManagerInterface $manager): Response
+    public function AjouterActuRequest(Request $request,  EntityManagerInterface $manager, SluggerInterface $slugger): Response
     {   $document = new Document();
         $form_document = $this->createForm(DocumentFormType::class,$document);
         $form_document -> handleRequest($request);
     
         if( $form_document->isSubmitted() && $form_document->isValid()){
+
+            $brochureFile = $form_document->get('PDF')->getData();
+
+            // this condition is needed because the 'brochure' field is not required
+            // so the PDF file must be processed only when a file is uploaded
+            if ($brochureFile) {
+                $originalFilename = pathinfo($brochureFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$brochureFile->guessExtension();
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $brochureFile->move(
+                        $this->getParameter('brochures_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+            }
             
             $manager->persist($document);
             $manager->flush();
--- src/Form/DocumentFormType.php
+++ src/Form/DocumentFormType.php
@@ -8,6 +8,9 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
 use App\Entity\Materiel;
 
 class DocumentFormType extends AbstractType
@@ -21,6 +24,29 @@ class DocumentFormType extends AbstractType
             ->add('mat', EntityType::class, [
                 'class' => Materiel::class,
                 'choice_label' => 'ref_mat',])
+            ->add("PDF", FileType::class, [
+                'label' => 'fichier PDF',
+    
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+    
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+    
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'application/pdf',
+                            'application/x-pdf',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid Image document',
+                    ])
+                ],
+            ])
         ;
     }
 
