201911939d4be77b3d7b94d8092c57c3c328f02c
k1llerhand19 <135227972+k1llerhand19@users.noreply.github.com>
2023-10-03 18:35:26 +0200

    rappeltout
    email fonctionne pas

---
+++ src/Controller/MailerController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+use App\Form\ContactType;
+
+
+
+class MailerController extends AbstractController
+{
+    #[Route('/admin/mailer', name: 'app_mailer')]
+    public function sendEmail(MailerInterface $mailer): Response
+    {
+        
+            $email = (new Email())
+            ->from('hello@example.com')
+            ->to('test@gmail.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+            $mailer->send($email);
+
+                                                                        
+
+        return $this->render('mailer/index.html.twig', [
+            'controller_name' => 'MailerController',
+            
+        ]);
+    }
+}
